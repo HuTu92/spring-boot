3add167d5e3efc6702e84ef477be03ebfd2cbea8
liuchunlong <631521383@qq.com>
2018-03-02 17:19:35 +0800

    Add ObjectProvider&HttpMessageConverter

--- src/main/java/com/github/fnpac/config/WebMvcConfig.java
+++ src/main/java/com/github/fnpac/config/WebMvcConfig.java
@@ -1,11 +1,20 @@
 package com.github.fnpac.config;
 
+import com.github.fnpac.pojo.DemoObj;
 import com.github.fnpac.spring.messageconverter.DividingMessageConverter;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.web.HttpMessageConverters;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.StringHttpMessageConverter;
+import org.springframework.http.converter.xml.SourceHttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 /**
@@ -49,7 +58,7 @@ public class WebMvcConfig extends WebMvcConfigurerAdapter {
 
     /**
      * spring boot 自动配置
-     *
+     * <p>
      * 用于Apache Commons FileUpload 1.2或更高版本的基于Servlet的MultipartResolver实现
      *
      * @return
@@ -64,9 +73,43 @@ public class WebMvcConfig extends WebMvcConfigurerAdapter {
     //////////////////////////////////////
     //      messageconverter
     //////////////////////////////////////
-
     @Override
     public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
         converters.add(new DividingMessageConverter());
+    }
+
+    /**
+     * spring boot customize config for HttpMessageConverter
+     *
+     * WebMvcAutoConfiguration will lazy autowires HttpMessageConverters
+     *      Autowire logic:
+     *          first by id;
+     *          then by type;
+     * so the bean name of HttpMessageConverters should be messageConverters，
+     * if not, there should be only one bean of HttpMessageConverters type.
+     *
+     * 配置HttpMessageConverters bean，会导致丢弃HttpMessageConvertersAutoConfiguration自动配置中通过构造函数注入的converters
+     */
+//    @Bean
+//    public HttpMessageConverters stringMessageConverters() {
+//        List<HttpMessageConverter<?>> converters = new ArrayList<>();
+//        StringHttpMessageConverter converter = new StringHttpMessageConverter();
+//        converter.setDefaultCharset(Charset.forName("utf-8"));
+//        converters.add(converter);
+//        // HttpMessageConverters 默认会添加 默认的HttpMessageConverter
+//        return new HttpMessageConverters(converters);
+//    }
+
+    //////////////////////////////////////
+    //      ObjectProvider demo
+    //////////////////////////////////////
+    @Bean
+    public DemoObj liucl() {
+        return new DemoObj("001", "liucl");
+    }
+
+    @Bean
+    public DemoObj lana() {
+        return new DemoObj("002", "lana");
     }
 }
---
+++ src/main/java/com/github/fnpac/spring/objectprovider/ObjectProviderDemo.java
@@ -0,0 +1,40 @@
+package com.github.fnpac.spring.objectprovider;
+
+import com.github.fnpac.pojo.DemoObj;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import java.util.List;
+import java.util.function.BiFunction;
+import java.util.function.BinaryOperator;
+
+/**
+ * Created by 刘春龙 on 2018/3/2.
+ */
+@Component
+public class ObjectProviderDemo {
+
+    private static final Logger logger = LoggerFactory.getLogger(ObjectProviderDemo.class);
+
+    private List<DemoObj> demoObjs;
+
+    public ObjectProviderDemo(
+            ObjectProvider<List<DemoObj>> demoObjsProvider) {
+        this.demoObjs = demoObjsProvider.getIfAvailable();
+    }
+
+    @PostConstruct
+    public void log() {
+        String info = "[";
+        info = demoObjs.stream().reduce(info,
+                (s, demoObj) -> s + ("(" + demoObj.getId() + ", " + demoObj.getName() + "), "),
+                (s, s2) -> null);
+//        2018-03-02 16:49:34.963  INFO 19804 --- [           main] c.g.f.s.o.ObjectProviderDemo             : [(001, liucl), (002, lana)]
+        info = info.subSequence(0, info.length() - 2).toString() + "]";
+        logger.info(info);
+    }
+
+}
